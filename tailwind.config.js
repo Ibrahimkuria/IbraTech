b03af6557b70cba42add371fd2e301d821bf610d
Ibrahim Kuria <kuriaibrahim507@gmail.com>
2025-09-27 17:48:10 +0300

    Initial portfolio upload (without node_modules)

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./project.htm", "./magical.htm","./first.html", "./portfolio.html"],   // 👈 Scan all HTML files in the root
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
